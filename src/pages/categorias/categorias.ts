10638dd2e6f8289e1f327fecc7a13e0576dbaccf
Isaque Lourenço <isaque.lourenco2@gmail.com>
2020-04-27 22:14:41 -0300

    Criando um interceptor para tratamentos de erros

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -32,9 +32,7 @@ export class CategoriasPage {
     this.categoriaService.findyAll()
     .subscribe(response => {this.items = response;
       },
-    error => {
-      console.log(error);
-    });
+    error => {});
 
   }
 }
