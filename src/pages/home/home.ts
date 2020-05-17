6e27652c3cb033fd04d349524eba7e52a4df22bc
Isaque Lourenço <isaque.lourenco2@gmail.com>
2020-05-16 21:19:40 -0300

    Criando página de Signup

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -46,8 +46,11 @@ export class HomePage {
         this.navCtrl.setRoot('CategoriasPage');
       }, 
       error => {});
+ 
+  }
 
-    
+  signup(){
+    this.navCtrl.push('SignupPage');
   }
 
 }
