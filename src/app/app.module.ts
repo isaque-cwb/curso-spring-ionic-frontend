4871e77c5a704ffe5f50c8343d32c04a424cfb37
Isaque Lourenço <isaque.lourenco2@gmail.com>
2020-05-19 12:36:24 -0300

    Salvando novo cliente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { MyApp } from './app.component';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { CategoriaService } from '../services/domain/categoria.service';
-import { ErrorInterceptorProvider } from '../interceptors/error.interceptors';
+import { ErrorInterceptorProvider } from '../interceptors/error-interceptor';
 import { AuthService } from '../services/auth.service';
 import { StorageService } from '../services/storage.service';
 import { ClienteService } from '../services/domain/cliente.service';
